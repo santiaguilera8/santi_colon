5423b48d5da16a7972ab245972fc51610c8df5f1
santiaguilera8 <santiipashion@gmail.com>
2022-11-24 19:50:48 -0300

    repositorio Sem6

---
+++ Clase 10/js/script.js
@@ -0,0 +1,31 @@
+
+let nombre = "Programación" + " Fácil.";
+let saludo = “Les doy la bienvenida a ";
+let fraseCompleta = saludo + nombre;
+console.log(fraseCompleta);
+
+//
+
+let frase = "JavaScript " + "es un lenguaje" + "esencial
+para crear" + " sitios web poderosos.";
+console.log(frase);
+
+//
+
+let numeroString1 = "10";
+let numeroString2 = "20";
+let numeroString3 = "30";
+let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+console.log(concatenaNumeros);
+
+//
+
+let edad;
+edad = prompt("Introduce tu edad");
+if (edad >= 18){
+alert("Puedes entrar, eres mayor de edad.");
+} else {
+alert("No puedes entrar, eres menor de edad.");
+} 
+
+
